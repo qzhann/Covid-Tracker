6290986fb46631fc34e143d4ad93a0154cf0dd16
Zihan Qi <qizihan0428@foxmail.com>
2022-01-04 22:50:30 -0800

    Initial Commit

---
+++ CovidTracker/CovidTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidTrackerApp.swift
+//  CovidTracker
+//
+//  Created by Zihan Qi on 1/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CovidTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
